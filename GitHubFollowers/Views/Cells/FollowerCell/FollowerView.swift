5f662d52133b1f0d9e593b7e717af4e1ccd8d584
SuichiNagi <aldreiglennkim@gmail.com>
2024-07-30 20:41:57 +0800

    Inject SwiftUI to UIKit

---
+++ GitHubFollowers/Views/Cells/FollowerCell/FollowerView.swift
@@ -0,0 +1,18 @@
+//
+//  FollowerView.swift
+//  GitHubFollowers
+//
+//  Created by Aldrei Glenn Nuqui on 7/30/24.
+//
+
+import SwiftUI
+
+struct FollowerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FollowerView()
+}
